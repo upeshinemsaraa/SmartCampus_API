d1820d64b356ecf39797ddae750e1dd2e6598730
NemSara <upeshinemsaraa@gmail.com>
2026-04-22 19:21:05 +0530

    add LinkedResourceNotFoundException with 422 mapper for invalid roomId

--- backend/SmartCampusAPI/src/main/java/com/smartcampus/config/ApplicationConfig.java
+++ backend/SmartCampusAPI/src/main/java/com/smartcampus/config/ApplicationConfig.java
@@ -2,7 +2,17 @@ package com.smartcampus.config;
 
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
+import org.glassfish.jersey.jackson.JacksonFeature;
+import java.util.HashSet;
+import java.util.Set;
 
 @ApplicationPath("/api/v1")
 public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(JacksonFeature.class);
+        return classes;
+    }
 }
---
+++ backend/SmartCampusAPI/src/main/java/com/smartcampus/mapper/LinkedResourceNotFoundExceptionMapper.java
@@ -0,0 +1,26 @@
+package com.smartcampus.mapper;
+
+import com.smartcampus.exception.LinkedResourceNotFoundException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
+
+@Provider
+public class LinkedResourceNotFoundExceptionMapper 
+        implements ExceptionMapper<LinkedResourceNotFoundException> {
+
+    @Override
+    public Response toResponse(LinkedResourceNotFoundException ex) {
+        Map<String, Object> error = new HashMap<>();
+        error.put("status", 422);
+        error.put("error", "Unprocessable Entity");
+        error.put("message", ex.getMessage());
+        return Response.status(422)
+                .entity(error)
+                .type(MediaType.APPLICATION_JSON)
+                .build();
+    }
+}
